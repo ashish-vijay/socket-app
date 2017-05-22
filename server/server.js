f1810ecc6b2ab4467c5f2ac73e2a35b9efaa42c8
zueore <zueore@gmail.com>
2017-05-22 22:34:07 +0530

    Heroku Deployment

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const {generateMessage} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('createMessage', (message) => {
+    console.log(message);
+    io.emit('newMessage', generateMessage(message.from, message.text));
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'User left'));
+  });
+
+});
+
+
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
